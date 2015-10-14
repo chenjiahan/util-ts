1518ed8c9252fd269b10525b53106cdbd73b85ed
chenjiahan <416417567@163.com>
2015-10-14 09:28:23 +0800

    parseHTML

---
+++ dom/parseHTML.js
@@ -0,0 +1,12 @@
+/**
+ * 解析html,返回element
+ * 若是多个DOM,则返回数组
+ */
+function parseHTML(str) {
+    var el = document.createElement('div');
+    el.innerHTML = str;
+    if(el.children.length === 1) {
+        return el.children[0];
+    }
+    return el.children;
+}
