7c9b02daaa56ce7cd084ec9e81104ae9cc457a12
陈嘉涵 <chenjiahan@meituan.com>
2016-12-30 19:36:24 +0800

    upd: clean files

--- array/duplicate.js
+++
@@ -1,8 +0,0 @@
-/**
- * 数组去重
- */
-export default function (arr) {
-    return arr.filter((value, index) => {
-        return arr.indexOf(value) === index
-    });
-}
--- utility/timestamp.js
+++
@@ -1,6 +0,0 @@
-/**
- * 时间戳,返回1970-01-01至今的毫秒数
- */
-export default function timestamp() {
-    return Date.now() || new Date().getTime();
-}
